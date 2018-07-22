77f16adde3654c5182fe5f7c7c5f97d65ab327e6
Alberto Kato <albertokatojr@gmail.com>
2018-07-22 20:21:36 -0300

    2740 - Liga

---
+++ 2740.sql
@@ -0,0 +1,23 @@
+/*
+Problem: Liga
+URI Online Judge | 2740
+Solution developed by: Alberto Kato
+*/
+
+(SELECT 
+    'Podium: ' || team AS name
+FROM
+    league
+ORDER BY position
+LIMIT 3) UNION ALL (SELECT 
+    'Demoted: ' || team AS name
+FROM
+    league
+WHERE
+    position IN (SELECT 
+            position
+        FROM
+            league
+        ORDER BY position DESC
+        LIMIT 2)
+ORDER BY position)
