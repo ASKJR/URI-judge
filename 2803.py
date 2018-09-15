02d712599785b32f5fa48a29f563bbf93e497098
Alberto Kato <albertokatojr@gmail.com>
2018-09-15 15:44:04 -0300

    2803 - Estados do Norte

---
+++ 2803.py
@@ -0,0 +1,11 @@
+'''
+Problem: Estados do Norte
+URI Online Judge | 2803
+Solution developed by: Alberto Kato
+'''
+northStates = ['roraima', 'acre', 'amapa', 'amazonas', 'para', 'rondonia', 'tocantins']
+
+if input() in northStates:
+    print('Regiao Norte')
+else:
+    print('Outra regiao')
