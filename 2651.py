4fb56527dd42872252c10711dce14dff22aaca77
Alberto Kato <albertokatojr@gmail.com>
2018-10-31 01:12:04 -0300

    2651 - Link Bolado

---
+++ 2651.py
@@ -0,0 +1,10 @@
+'''
+Problem: Link Bolado
+URI Online Judge | 2651
+Solution developed by: Alberto Kato
+'''
+S = input().lower()
+if "zelda" in S:
+    print("Link Bolado")
+else:
+    print("Link Tranquilo")
