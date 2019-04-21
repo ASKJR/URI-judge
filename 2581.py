1ac2f26a49e875832fc7bbba429c8a85f28a75f1
ASKJR <albertokatojr@gmail.com>
2019-04-21 17:15:37 -0300

    2581 - I am Toorg!

---
+++ 2581.py
@@ -0,0 +1,13 @@
+'''
+Problem: I am Toorg!
+URI Online Judge | 2581
+Solution developed by: Alberto Kato
+'''
+from sys import stdin, stdout
+
+
+loop = int(stdin.readline() + '\n')
+
+for i in range(0, loop):
+    q = str(stdin.readline())
+    stdout.write('I am Toorg!\n')
