32b3aa140a8b49e54dce2cddba905ef3446e4be6
Alberto Kato <albertokatojr@gmail.com>
2022-03-20 14:33:17 -0300

    [C#] Keanu - 3342

---
+++ C-Sharp/3342-Keanu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Keanu
+     *  beecrowd | 3342
+     *  Solution developed by: Alberto Kato
+     */
+    static void Main(string[] args)
+    {
+        int whiteSquares = 0 , blackSquares = 0;
+        int boardDimension = int.Parse(Console.ReadLine().Trim());
+        int boardSize = (boardDimension * boardDimension);
+        whiteSquares += (boardSize % 2) + (boardSize / 2);
+        blackSquares += (boardSize / 2);
+        Console.WriteLine("{0} casas brancas e {1} casas pretas", whiteSquares, blackSquares);
+    }
+}
