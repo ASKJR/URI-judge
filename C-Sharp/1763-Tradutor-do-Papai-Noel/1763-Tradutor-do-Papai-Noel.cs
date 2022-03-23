4b7959fa82ae4b481542b765515eb9ae4a3b9dd6
Alberto Kato <albertokatojr@gmail.com>
2022-03-22 21:38:05 -0300

    [C#] 1763 - Tradutor do Papai Noel

---
+++ C-Sharp/1763-Tradutor-do-Papai-Noel/1763-Tradutor-do-Papai-Noel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+class URI
+{
+    /*  
+     *  Problem: Tradutor do Papai Noel
+     *  beecrowd | 1763
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> christmasGreetingsByCountry = new Dictionary<string, string>()
+        {
+            {"brasil"         ,"Feliz Natal!"},
+            {"alemanha"       ,"Frohliche Weihnachten!"},
+            {"austria"        ,"Frohe Weihnacht!"},
+            {"coreia"         ,"Chuk Sung Tan!"},
+            {"espanha"        ,"Feliz Navidad!"},
+            {"grecia"         ,"Kala Christougena!"},
+            {"estados-unidos" ,"Merry Christmas!"},
+            {"inglaterra"     ,"Merry Christmas!"},
+            {"australia"      ,"Merry Christmas!"},
+            {"portugal"       ,"Feliz Natal!"},
+            {"suecia"         ,"God Jul!"},
+            {"turquia"        ,"Mutlu Noeller"},
+            {"argentina"      ,"Feliz Navidad!"},
+            {"chile"          ,"Feliz Navidad!"},
+            {"mexico"         ,"Feliz Navidad!"},
+            {"antardida"      ,"Merry Christmas!"},
+            {"canada"         ,"Merry Christmas!"},
+            {"irlanda"        ,"Nollaig Shona Dhuit!"},
+            {"belgica"        ,"Zalig Kerstfeest!"},
+            {"italia"         ,"Buon Natale!"},
+            {"libia"          ,"Buon Natale!"},
+            {"siria"          ,"Milad Mubarak!"},
+            {"marrocos"       ,"Milad Mubarak!"},
+            {"japao"          ,"Merii Kurisumasu!"}
+        };
+
+        string country;
+
+        while((country = Console.ReadLine()) != null)
+        {
+            if(christmasGreetingsByCountry.ContainsKey(country))
+            {
+                Console.WriteLine(christmasGreetingsByCountry[country]);
+            }
+            else
+            {
+                Console.WriteLine("--- NOT FOUND ---");
+            }
+        }
+    }
+}
