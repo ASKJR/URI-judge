d536c19ec30e67846dbee7a5e17652f467dede06
Alberto Kato <albertokatojr@gmail.com>
2022-03-23 22:30:33 -0300

    [C#] 2926 - Então é Natal!

---
+++ C-Sharp/2926-Entao-e-Natal/2926-Entao-e-Natal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+class URI
+{
+    /*  
+     *  Problem: Então é Natal!
+     *  beecrowd | 2926
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int repeatLetterNTimes = int.Parse(Console.ReadLine());
+        Console.WriteLine(new StringBuilder("Entao eh Natal!")
+            .Replace("a", new String('a', repeatLetterNTimes))
+            .ToString());
+    }
+}
