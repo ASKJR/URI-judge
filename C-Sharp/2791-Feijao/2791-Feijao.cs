c015cd55c6cb0013ea025f15ccb7492977fb4fcf
Alberto Kato <albertokatojr@gmail.com>
2022-03-23 21:34:14 -0300

    [C#] 2791- Feijão

---
+++ C-Sharp/2791-Feijao/2791-Feijao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Feijão
+     *  beecrowd | 2791
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Trim().Split(' ');
+        Console.WriteLine(Array.FindIndex(input, item => item == "1") + 1);
+    }
+}
