5762e90d265828baafffd2d9684a739e6ac92fce
Alberto Kato <albertokatojr@gmail.com>
2022-03-25 22:27:51 -0300

    [C#] 3301- Sobrinho do Meio!

---
+++ C-Sharp/Sobrinho-do-Meio/Sobrinho-do-Meio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Sobrinho do Meio!
+     *  beecrowd | 3301
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string[] names = new string[] { "huguinho", "zezinho", "luisinho" };
+        int[] ages = Console.ReadLine().Split(' ').Select(age => int.Parse(age)).ToArray();
+        var namesAndAges = names
+            .Zip(ages, (name,age) => new { name, age })
+            .OrderBy(nameAndAge => nameAndAge.age)
+            .ToArray();
+
+        Console.WriteLine(namesAndAges[1].name);
+    }
+}
