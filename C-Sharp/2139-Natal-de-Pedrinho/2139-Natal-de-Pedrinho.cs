c561aba8b09f5e61c07202367ed0f2050335f68b
Alberto Kato <albertokatojr@gmail.com>
2022-03-28 23:00:22 -0300

    [C#] 2139- Natal de Pedrinho

---
+++ C-Sharp/2139-Natal-de-Pedrinho/2139-Natal-de-Pedrinho.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Natal de Pedrinho
+     *  beecrowd | 2139
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        DateTime currentDay;
+        DateTime christmasDay = new DateTime(2016, 12, 25);
+        string dateInput;
+        while ((dateInput = Console.ReadLine()) !=null)
+        {
+            string[] dateInputArr = dateInput.Split(' ');
+            currentDay = new DateTime(2016, int.Parse(dateInputArr[0]), int.Parse(dateInputArr[1]));
+            int daysDifference = (christmasDay.Date - currentDay.Date).Days;
+            if (daysDifference < 0)
+                Console.WriteLine("Ja passou!");
+            else if (daysDifference == 0)
+                Console.WriteLine("E natal!");
+            else if (daysDifference == 1)
+                Console.WriteLine("E vespera de natal!");
+            else
+                Console.WriteLine("Faltam {0} dias para o natal!", daysDifference);
+        }
+    }
+}
