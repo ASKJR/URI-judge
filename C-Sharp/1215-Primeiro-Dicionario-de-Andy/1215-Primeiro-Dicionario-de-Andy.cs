dd7f2c40e1201ca7ce7e9a3847899ec7a757139d
Alberto Kato <albertokatojr@gmail.com>
2022-03-28 22:16:23 -0300

    [C#] 1215- Primeiro Dicionário de Andy

---
+++ C-Sharp/1215-Primeiro-Dicionario-de-Andy/1215-Primeiro-Dicionario-de-Andy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class URI
+{
+    /*  
+     *  Problem: Primeiro Dicionário de Andy
+     *  beecrowd | 1215
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+
+        string phrase;
+        SortedSet<string> wordsSet = new SortedSet<string>();
+
+        while ((phrase = Console.ReadLine()) != null)
+        {
+            if (phrase != "")
+            {
+                var words = phrase.Split(' ');
+                foreach (var word in words)
+                {
+
+                    string newString = "";
+                    foreach (var letter in word)
+                    {
+                        if (!((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z')))
+                        {
+                            newString += "_";
+                        } 
+                        else
+                        {
+                            newString += letter;
+                        }
+                    }
+                    var newStrings = newString.Split('_');
+                    foreach (var item in newStrings)
+                    {
+                        wordsSet.Add(item.ToLower());
+                    }
+                    
+                }
+            }
+        }
+        foreach (var item in wordsSet)
+        {
+            if(item != "")
+                Console.WriteLine(item);
+        }
+
+    }
+}
