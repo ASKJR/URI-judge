f8541874474e20024ced7e01d738be661d616bca
Alberto Kato <albertokatojr@gmail.com>
2022-04-03 13:00:01 -0300

    [C#] 1273 -  Justificador

---
+++ C-Sharp/1273-Justificador/1273-Justificador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class URI
+{
+    /*  
+     *  Problem: Justificador
+     *  beecrowd | 1273
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string N;
+        string word;
+        int space = 0;
+        while ((N = Console.ReadLine()) != "0")
+        {
+            if (space > 0) Console.WriteLine();
+            int casesNum = int.Parse(N);
+            int max = 0;
+            List<string> words = new List<string>();
+            for (int i = 0; i < casesNum; i++)
+            {
+                word = Console.ReadLine();
+                if (word.Length > max)
+                {
+                    max = word.Length;
+                }
+                words.Add(word);
+            }
+            foreach (var w in words)
+            {
+                Console.WriteLine(w.PadLeft(max));
+            }
+            space++;
+        }
+    }
+}
