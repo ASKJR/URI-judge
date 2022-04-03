c1b2a069a67f8769e2bb78fb71f785e7200a72cd
Alberto Kato <albertokatojr@gmail.com>
2022-04-03 00:28:59 -0300

    [C#] 2322- Peça Perdida

---
+++ C-Sharp/2322-Peca-Perdida/2322-Peca-Perdida.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Peça Perdida
+     *  beecrowd | 2322
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        IEnumerable<int> allPieces = Enumerable.Range(1, N);
+        IEnumerable<int> myPieces = Console.ReadLine().Split(' ').Select(piece => int.Parse(piece));
+        Console.WriteLine(allPieces.Except(myPieces).First());
+    }
+}
