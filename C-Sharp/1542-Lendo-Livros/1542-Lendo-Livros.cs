a691b0e2f31196c464409ef71f73fe1379af142e
Alberto Kato <albertokatojr@gmail.com>
2022-04-02 23:44:37 -0300

    [C#] 1542 - Lendo Livros

---
+++ C-Sharp/1542-Lendo-Livros/1542-Lendo-Livros.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Lendo Livros
+     *  beecrowd | 1542
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string[] input;
+        string pluralCase;
+        float Q, D, P;
+        int output;
+
+        while (true)
+        {
+            input = Console.ReadLine().Split(' ');
+            if (input[0] == "0") break;
+            Q = float.Parse(input[0]);
+            D = float.Parse(input[1]);
+            P = float.Parse(input[2]);
+            output = (int)(((P * D) / (P - Q)) * Q);
+            pluralCase = output > 1 ? " paginas" : " pagina";
+            Console.WriteLine(output + pluralCase);
+        }
+    }
+}
