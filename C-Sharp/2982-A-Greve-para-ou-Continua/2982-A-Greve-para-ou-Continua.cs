d0df91ea57622a776076fef2ab9997923261dc1d
Alberto Kato <albertokatojr@gmail.com>
2022-04-04 23:33:56 -0300

    [C#] 2982 -  A Greve para ou Continua?

---
+++ C-Sharp/2982-A-Greve-para-ou-Continua/2982-A-Greve-para-ou-Continua.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: A Greve para ou Continua?
+     *  beecrowd | 2982
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int G = 0, V = 0;
+        string[] input; 
+        string type;
+        while (N > 0)
+        {
+            input = Console.ReadLine().Split(' ');
+            type = input[0];
+            if (type == "G")
+            {
+                G += int.Parse(input[1]);
+            }
+            else
+            {
+                V += int.Parse(input[1]);
+            }
+            N--;
+        }
+        if (V >= G)
+        {
+            Console.WriteLine("A greve vai parar.");
+        }
+        else
+        {
+            Console.WriteLine("NAO VAI TER CORTE, VAI TER LUTA!");
+        }
+    }
+}
