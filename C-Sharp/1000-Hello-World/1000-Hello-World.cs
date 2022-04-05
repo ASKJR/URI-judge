bdfa588da170f4fb742ca7a7e57b97c2b816b5a1
Alberto Kato <albertokatojr@gmail.com>
2022-04-04 23:49:43 -0300

    [C#] 1000 -  Hello World!

---
+++ C-Sharp/1000-Hello-World/1000-Hello-World.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Hello World!
+     *  beecrowd | 1000
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
