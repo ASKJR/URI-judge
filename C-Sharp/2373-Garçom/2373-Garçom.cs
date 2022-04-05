760a609c7a3c6bbbfe672263b8e3b2e15ed6e293
Alberto Kato <albertokatojr@gmail.com>
2022-04-04 22:57:08 -0300

    [C#] 2373 - Garçom

---
+++ C-Sharp/2373-Garçom/2373-Garçom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Garçom
+     *  beecrowd | 2373
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int brokenCount = 0;
+        int[] input;
+
+        for (int i = 0; i < N; i++)
+        {
+            input = Console.ReadLine().Split(' ').Select(n => int.Parse(n)).ToArray();
+            if(input[0] > input[1])
+            {
+                brokenCount+= input[1];
+            }
+        }
+
+        Console.WriteLine(brokenCount);
+    }
+}
