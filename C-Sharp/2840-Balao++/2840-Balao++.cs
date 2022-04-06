2051f32c7cb2d7ac587534545021a939165fbb2b
Alberto Kato <albertokatojr@gmail.com>
2022-04-06 19:27:23 -0300

    [C#] 2840 -  Balão++

---
+++ C-Sharp/2840-Balao++/2840-Balao++.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Balão++
+     *  beecrowd | 2840
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        double PI = 3.1415;
+        string[] input = Console.ReadLine().Split(' ');
+        int radius = int.Parse(input[0]);
+        int availableHe = int.Parse(input[1]);  
+        double sphereV = (4 * PI * (radius * radius * radius)) / 3;
+        Console.WriteLine((int)(availableHe/sphereV));
+    }
+}
