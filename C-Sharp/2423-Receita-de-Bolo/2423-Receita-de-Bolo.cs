1f08fbb6fe048abc0329d7702c4f9e90e90f3de6
Alberto Kato <albertokatojr@gmail.com>
2022-04-06 22:31:58 -0300

    [C#] 2423 -  Receita de Bolo

---
+++ C-Sharp/2423-Receita-de-Bolo/2423-Receita-de-Bolo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Receita de Bolo
+     *  beecrowd | 2423
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split(' ').Select(n => int.Parse(n)).ToArray();
+        input[0] = input[0]/2;
+        input[1] = input[1]/3;
+        input[2] = input[2]/5;
+        Console.WriteLine(input.Min());
+    }
+}
