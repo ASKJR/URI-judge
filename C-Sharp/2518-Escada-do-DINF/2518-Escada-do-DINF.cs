93652a7f69007aad75516a12f2600484b8722878
Alberto Kato <albertokatojr@gmail.com>
2022-04-06 21:18:48 -0300

    [C#] 2518 -  Escada do DINF

---
+++ C-Sharp/2518-Escada-do-DINF/2518-Escada-do-DINF.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Escada do DINF
+     *  beecrowd | 2518
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string line;
+        string[] input;
+        int N, H, C, L;
+        double hip, stairsArea;
+        while((line = Console.ReadLine()) != null)
+        {
+            input = Console.ReadLine().Split(' ');
+            N = int.Parse(line);
+            H = int.Parse(input[0]);
+            C = int.Parse(input[1]);
+            L = int.Parse(input[2]);
+            hip = Math.Sqrt((H*H)+(C*C));
+            stairsArea = ((hip * N) * L) / 10000;
+            Console.WriteLine(String.Format("{0:0.0000}", stairsArea));
+        }
+    }
+}
