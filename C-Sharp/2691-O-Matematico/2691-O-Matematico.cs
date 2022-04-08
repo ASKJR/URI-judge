61d055ffd9b8352175851edd4cc6885547a28d64
Alberto Kato <albertokatojr@gmail.com>
2022-04-07 22:28:30 -0300

    [C#] 2691 -  O Matemático

---
+++ C-Sharp/2691-O-Matematico/2691-O-Matematico.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: O Matemático
+     *  beecrowd | 2691
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string[] line;
+        int X, Y;
+        for (int i = 0; i < N; i++)
+        {
+            line = Console.ReadLine().Split('x');
+            X = int.Parse(line[0]);
+            Y = int.Parse(line[1]);
+
+            for (int j = 5; j <= 10; j++)
+            {
+                if (X==Y)
+                {
+                    Console.WriteLine($"{X} x {j} = {X*j}");
+                } 
+                else
+                {
+                    Console.WriteLine($"{X} x {j} = {X * j} && {Y} x {j} = {Y * j}");
+                }
+            }
+        }
+    }
+}
