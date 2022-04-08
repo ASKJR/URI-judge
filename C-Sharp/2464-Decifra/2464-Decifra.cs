9d88bab6a41f4436d8e1881a875729c3f4ec7405
Alberto Kato <albertokatojr@gmail.com>
2022-04-07 22:51:24 -0300

    [C#] 2464 - Decifra

---
+++ C-Sharp/2464-Decifra/2464-Decifra.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Decifra
+     *  beecrowd | 2464
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        string rule = Console.ReadLine();
+        string toDecode = Console.ReadLine();
+        string decoded = "";
+        
+        foreach(char c in toDecode)
+        {
+            decoded+= rule[alphabet.IndexOf(c)];
+        }
+
+        Console.WriteLine(decoded);
+    }
+}
