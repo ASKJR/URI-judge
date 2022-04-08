c01d08036090dcdd42c46f9afc2dc0e638cb927a
Alberto Kato <albertokatojr@gmail.com>
2022-04-07 21:55:17 -0300

    [C#] 1214 -  Acima da Média

---
+++ C-Sharp/1214-Acima-da-Media/1214-Acima-da-Media.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Acima da Média
+     *  beecrowd | 1214
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int C = int.Parse(Console.ReadLine());
+        IEnumerable<decimal> input;
+        decimal avg, aboveAvgPercent, first;
+        for (int i = 0; i < C; i++)
+        {
+            input = Console.ReadLine().Split(' ').Select(n => decimal.Parse(n));
+            first = input.First();
+            input = input.Skip(1);
+            avg = input.Average();
+            aboveAvgPercent = ((input.Where(n => n > avg).Count()) / first)*100;
+            Console.WriteLine(String.Format("{0:0.000}%", Math.Round(aboveAvgPercent,3)));
+        }
+    }
+}
