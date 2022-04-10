d5cb80b236310b8cdb90208fa481985d200674db
Alberto Kato <albertokatojr@gmail.com>
2022-04-10 20:43:03 -0300

    [C#] 3299 - Números Má Sorte Pequenos

---
+++ C-Sharp/3299-Números-Má-Sorte-Pequenos/3299-Números-Má-Sorte-Pequenos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Números Má Sorte Pequenos
+     *  beecrowd | 3299
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        if (input.Contains("13"))
+        {
+            Console.WriteLine($"{input} es de Mala Suerte");
+        } 
+        else
+        {
+            Console.WriteLine($"{input} NO es de Mala Suerte");
+        }
+    }
+}
