c97c32e7467cde04cc5194e8154d6367975d6935
Alberto Kato <albertokatojr@gmail.com>
2022-04-09 23:48:53 -0300

    [C#] 2582 - System of a Download

---
+++ C-Sharp/2582-System-of-a-Download/2582-System-of-a-Download.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class URI
+{
+    /*  
+     *  Problem: System of a Download
+     *  beecrowd | 2582
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string[] ids;
+        int code;
+        Dictionary<int,string> musics = new Dictionary<int, string> {
+            {0 ,  "PROXYCITY"},
+            {1 ,  "P.Y.N.G."},
+            {2 ,  "DNSUEY!"},
+            {3 ,  "SERVERS"},
+            {4 ,  "HOST!"},
+            {5 ,  "CRIPTONIZE"},
+            {6 ,  "OFFLINE DAY"},
+            {7 ,  "SALT"},
+            {8 ,  "ANSWER!"},
+            {9 ,  "RAR?"},
+            {10,  "WIFI ANTENNAS"},
+        };   
+        for (int i = 0; i < N; i++)
+        {
+            ids = Console.ReadLine().Split(' ');
+            code = int.Parse(ids[0]) + int.Parse(ids[1]);
+            Console.WriteLine(musics[code]);
+        }
+    }
+}
