062a43ef2ce37e273e94347359ee9ab61f537bb7
Alberto Kato <albertokatojr@gmail.com>
2022-04-10 20:28:16 -0300

    [C#] 3303 - Palavrão

---
+++ C-Sharp/3303-Palavrão/3303-Palavrão.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Palavrão
+     *  beecrowd | 3303
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+
+        string line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            Console.WriteLine((line.Length >= 10 ? "palavrao" : "palavrinha"));
+        }
+    } 
+}
