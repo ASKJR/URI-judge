5ff6041667527131cab8125779ae810f6828d099
Alberto Kato <albertokatojr@gmail.com>
2022-04-11 19:19:40 -0300

    [C#] 3039 - Brinquedos do Papai Noel

---
+++ C-Sharp/3039-Brinquedos-do-Papai-Noel/3039-Brinquedos-do-Papai-Noel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Brinquedos do Papai Noel
+     *  beecrowd | 3039
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int dolls = 0, cars = 0;
+        string g;
+        
+        for (int i = 0; i < N; i++)
+        {
+            g = Console.ReadLine().Split(' ')[1];
+            if (g == "M") ++cars;
+            else ++dolls;
+        }
+
+        Console.WriteLine($"{cars} carrinhos");
+        Console.WriteLine($"{dolls} bonecas");
+    }
+}
