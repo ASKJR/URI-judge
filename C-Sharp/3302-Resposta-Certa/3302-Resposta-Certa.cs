8bf483a772b732337bcfc0a9b1e3c33bc4d3f274
Alberto Kato <albertokatojr@gmail.com>
2022-04-11 18:34:22 -0300

    [C#] 3302- Resposta Certa

---
+++ C-Sharp/3302-Resposta-Certa/3302-Resposta-Certa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Resposta Certa
+     *  beecrowd | 3302
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string line, answer;
+        int N;
+        
+        while ((line = Console.ReadLine()) != null)
+        {
+            N = int.Parse(line);
+            for (int i = 1; i <= N; i++)
+            {
+                answer = Console.ReadLine();
+                Console.WriteLine($"resposta {i}: {answer}");
+            }
+        }
+    }
+}
