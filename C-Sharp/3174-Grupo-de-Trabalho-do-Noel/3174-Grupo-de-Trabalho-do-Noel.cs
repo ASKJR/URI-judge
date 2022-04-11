ac9826b715abe1d4dc62d2fc99ad1256114897a5
Alberto Kato <albertokatojr@gmail.com>
2022-04-11 20:33:59 -0300

    [C#] 3174 - Grupo de Trabalho do Noel

---
+++ C-Sharp/3174-Grupo-de-Trabalho-do-Noel/3174-Grupo-de-Trabalho-do-Noel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Grupo de Trabalho do Noel
+     *  beecrowd | 3174
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int B = 0, A = 0, M = 0, D = 0, hours, toys;
+        string[] line; 
+        string type;
+        for (int i = 0; i < N; i++)
+        {
+            line = Console.ReadLine().Split(' ');
+            type = line[1];
+            hours = int.Parse(line[2]);
+            
+            if (type == "bonecos")
+            {
+                B += hours;
+            } 
+            else if (type == "arquitetos")
+            {
+                A += hours;
+            }
+            else if (type == "musicos")
+            {
+                M += hours;
+            }
+            else
+            {
+                D += hours;
+            }
+        }
+        
+        toys = (B/8) + (A/4) + (M/6) + (D/12);
+        
+        Console.WriteLine(toys);
+    }
+}
