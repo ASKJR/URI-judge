579cc837159bbdc83d4f6cf893ba484e05f0db20
Alberto Kato <albertokatojr@gmail.com>
2022-04-11 19:40:08 -0300

    [C#] 3157 - Viva a Ciência!

---
+++ C-Sharp/3157-Viva-a-Ciência!/3157-Viva-a-Ciência!.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Viva a Ciência!
+     *  beecrowd | 3157
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string line = Console.ReadLine();
+        Console.WriteLine("Nao se refuta ciencia com opiniao");
+    }
+}
