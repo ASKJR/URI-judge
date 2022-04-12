27aaf3d8148d0e0e802bb72edcdbf871039325df
Alberto Kato <albertokatojr@gmail.com>
2022-04-12 20:56:34 -0300

    [C#] 3224 - Aaah!

---
+++ C-Sharp/3224-Aaah!/3224-Aaah!.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Aaah!
+     *  beecrowd | 3224
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string jon = Console.ReadLine();
+        string doc = Console.ReadLine();
+        if (jon.Length >= doc.Length) Console.WriteLine("go");
+        else Console.WriteLine("no");
+    }
+}
