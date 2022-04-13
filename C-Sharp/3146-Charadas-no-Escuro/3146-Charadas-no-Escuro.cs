0d202a44a7d7cc6e0a09fb645b28adb0c1e85690
Alberto Kato <albertokatojr@gmail.com>
2022-04-12 21:26:30 -0300

    [C#] 3146 - Charadas no Escuro

---
+++ C-Sharp/3146-Charadas-no-Escuro/3146-Charadas-no-Escuro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Charadas no Escuro
+     *  beecrowd | 3146
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        double r = double.Parse(Console.ReadLine());
+        Console.WriteLine(String.Format("{0:0.00}", Math.Round(r * 6.28, 2)));
+    }
+}
