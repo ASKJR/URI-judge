b8d69f4d255259cf086738fd7f2cd25d5b5853db
Alberto Kato <albertokatojr@gmail.com>
2022-04-21 01:20:08 -0300

    [C#] 2457 - Letras

---
+++ C-Sharp/2457-Letras/2457-Letras.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Letras
+     *  beecrowd | 2457
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string letter = Console.ReadLine();
+        string[] phrase = Console.ReadLine().Split(' ');
+        float letterCount = phrase.Where(word => word.Contains(letter)).Count();
+        Console.WriteLine(String.Format("{0:0.0}", (letterCount/phrase.Length)*100 ));
+    }
+}
