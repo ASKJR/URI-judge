2ee5437568bc917cffe3242cb0e834d569c1ccdb
Alberto Kato <albertokatojr@gmail.com>
2022-04-21 00:06:22 -0300

    [C#] - 2444 - Volume da TV

---
+++ C-Sharp/2444-Volume-da-TV/2444-Volume-da-TV.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Volume da TV
+     *  beecrowd | 2444
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int[] input1 = Console.ReadLine().Split(' ').Select(n => int.Parse(n)).ToArray();
+        int[] input2 = Console.ReadLine().Split(' ').Select(n => int.Parse(n)).ToArray();
+        int volume = input1[0];
+        
+        for (int i = 0; i < input2.Length; i++)
+        {
+            volume += input2[i];
+            if (volume > 100) volume = 100;
+            if (volume < 0) volume = 0;
+        }
+
+        Console.WriteLine(volume);
+    }
+}
