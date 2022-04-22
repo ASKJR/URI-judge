a789287d65d4d81bb270a3b732dfd26d08de4b2d
Alberto Kato <albertokatojr@gmail.com>
2022-04-22 14:28:58 -0300

    [C#] 1805 - Soma Natural

---
+++ C-Sharp/1805-Soma-Natural/1805-Soma-Natural.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Soma Natural
+     *  beecrowd | 1805 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        
+        long[] interval = Console.ReadLine().Split(' ').Select(n => long.Parse(n)).ToArray();
+        Console.WriteLine((((interval[1]-interval[0])+1)*(interval[0] + interval[1]))/2);
+    }
+}
