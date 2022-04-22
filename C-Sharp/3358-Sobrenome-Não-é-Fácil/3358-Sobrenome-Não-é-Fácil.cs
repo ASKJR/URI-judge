803b216f0a2aba7d98338560014428c45be73ba1
Alberto Kato <albertokatojr@gmail.com>
2022-04-22 20:44:57 -0300

    [C#] 3358 - Sobrenome Não é Fácil

---
+++ C-Sharp/3358-Sobrenome-Não-é-Fácil/3358-Sobrenome-Não-é-Fácil.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Sobrenome Não é Fácil
+     *  beecrowd | 3358 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string surname;
+
+        for (int i = 0; i < N; i++)
+        {
+            surname = Console.ReadLine();
+            if (!isHard(surname))
+            {
+
+                Console.WriteLine($"{surname} eh facil");
+            } 
+            else
+            {
+                Console.WriteLine($"{surname} nao eh facil");
+            }
+        }
+    }
+
+    public static bool isHard(string surname)
+    {
+        char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+        int countConsonant = 0;
+
+        foreach (char letter in surname)
+        {
+            if(!vowels.Contains(letter))
+            {
+                ++countConsonant;
+            } 
+            else
+            {
+                countConsonant = 0;
+            }
+
+            if (countConsonant == 3) break;
+        }
+
+        return countConsonant == 3;
+    }
+}
