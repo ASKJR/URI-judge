a7925d7c012d72d1c3a02dd999fae4761a72b81e
Alberto Kato <albertokatojr@gmail.com>
2022-04-22 01:39:22 -0300

    [C#] 3091 - Resto 1.0

---
+++ C-Sharp/3091-Resto-1.0/3091-Resto-1.0.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Resto 1.0
+     *  beecrowd | 3091
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        Console.WriteLine(A%B);
+    }
+
+}
