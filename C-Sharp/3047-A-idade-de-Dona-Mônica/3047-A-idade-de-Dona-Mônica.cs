25fd386b421feb7c4818b6349b545276d32fa1c3
Alberto Kato <albertokatojr@gmail.com>
2022-04-22 02:02:22 -0300

    [C#] 3047 - A idade de Dona Mônica

---
+++ C-Sharp/3047-A-idade-de-Dona-Mônica/3047-A-idade-de-Dona-Mônica.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: A idade de Dona Mônica
+     *  beecrowd | 3047
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int M = int.Parse(Console.ReadLine());
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        int C = M - (A + B);
+        int[] ages = new int[] { A, B, C };
+        Console.WriteLine(ages.Max());
+    }
+
+}
