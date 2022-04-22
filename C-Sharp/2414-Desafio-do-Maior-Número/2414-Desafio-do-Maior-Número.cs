667fa11d64bc4722e079c74c0593f119cb3d5aaf
Alberto Kato <albertokatojr@gmail.com>
2022-04-22 20:20:41 -0300

    [C#] 2414 - Desafio do Maior Número

---
+++ C-Sharp/2414-Desafio-do-Maior-Número/2414-Desafio-do-Maior-Número.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Desafio do Maior Número
+     *  beecrowd | 2414 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Console.ReadLine().Split(' ').Select(n => int.Parse(n)).Max()); 
+    }
+}
