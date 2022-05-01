868cf6be1d0fdb94e6025a87985fcb441365194d
Alberto Kato <albertokatojr@gmail.com>
2022-05-01 20:32:56 -0300

    [C#] 3088 - Correção de Texto

---
+++ C-Sharp/3088-Correcao-de-Texto/3088-Correcao-de-Texto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Correção de Texto
+     *  beecrowd | 3088 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string line;
+        string output;
+
+        while ((line = Console.ReadLine()) != null)
+        {
+            output = line.Replace(" ,", ",");
+            Console.WriteLine(output.Replace(" .", "."));
+        }
+    }
+
+}
