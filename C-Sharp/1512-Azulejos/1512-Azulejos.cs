128f841413fa777e287dede1f7d564ad0ae186e3
Alberto Kato <albertokatojr@gmail.com>
2022-05-01 19:41:51 -0300

    [C#] 3170 - Bolinhas de Natal

---
+++ C-Sharp/1512-Azulejos/1512-Azulejos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Azulejos
+     *  beecrowd | 1512 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string[] input;
+        int N, A, B, lastDivisibleA, firstDivisibleA, lastDivisibleB, firstDivisibleB, sumDivisible;
+        
+        for (; ;)
+        {
+            input = Console.ReadLine().Split(' ');
+            N = int.Parse(input[0]);
+            A = int.Parse(input[1]);
+            B = int.Parse(input[2]);
+            
+            if (N == 0 && A == 0 && B == 0) break;
+
+            lastDivisibleA = GetLastMultiple(N, A, B);
+            lastDivisibleB = GetLastMultiple(N, B, B);
+            firstDivisibleA = A;
+            firstDivisibleB = B;
+            sumDivisible = GetHowManyDivisible(firstDivisibleA, lastDivisibleA, A) + GetHowManyDivisible(firstDivisibleB, lastDivisibleB, B);
+            
+            Console.WriteLine(sumDivisible);
+        }
+    }
+    static int GetHowManyDivisible(int firstDivisible, int lastDivisible, int divisibleBy)
+    {
+        return ((lastDivisible - firstDivisible) / divisibleBy) + 1;
+    }
+
+    static int GetLastMultiple(int N, int A, int B)
+    {
+        for (int i = (N-1); i >= 0; i--)
+        {
+            if (i % A == 0 || i % B == 0) return i;
+        }
+        return -1;
+    }
+
+}
---
+++ C-Sharp/3170-Bolinhas-de-Natal/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Bolinhas de Natal
+     *  beecrowd | 3170 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int B, G;
+        int output;
+        B = int.Parse(Console.ReadLine());
+        G = int.Parse(Console.ReadLine());
+        output = (G / 2) - B;
+        if (output <= 0) Console.WriteLine("Amelia tem todas bolinhas!");
+        else Console.WriteLine($"Faltam {output} bolinha(s)");
+    }
+   
+}
