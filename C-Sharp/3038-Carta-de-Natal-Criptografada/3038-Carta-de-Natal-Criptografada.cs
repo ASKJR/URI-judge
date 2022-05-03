4a62586fbe629940e02e42b8d3281f563025e905
Alberto Kato <albertokatojr@gmail.com>
2022-05-02 22:37:18 -0300

    [C#] 3038 - Carta de Natal Criptografada

---
+++ C-Sharp/3038-Carta-de-Natal-Criptografada/3038-Carta-de-Natal-Criptografada.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Carta de Natal Criptografada
+     *  beecrowd | 3038 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string line;
+
+        while ((line = Console.ReadLine()) != null)
+        {
+            line = line
+             .Replace("@", "a")
+             .Replace("&", "e")
+             .Replace("!", "i")
+             .Replace("*", "o")
+             .Replace("#", "u");
+
+            Console.WriteLine(line);
+        }
+    }
+
+}
