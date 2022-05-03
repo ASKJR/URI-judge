c816dda7d68229203db3357bcb0eded299030c5d
Alberto Kato <albertokatojr@gmail.com>
2022-05-02 23:35:02 -0300

    [C#] 2557 - R+L=J

---
+++ C-Sharp/2557-R+L=J/2557-R+L=J.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: R+L=J
+     *  beecrowd | 2557 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string line, R, L, J;
+        string[] input;
+
+
+        while ((line = Console.ReadLine()) != null)
+        {
+            input = line.Split('+'); //input[0] R
+            R = input[0];
+            input = input[1].Split('=');
+            L = input[0];
+            J = input[1];
+
+
+            if (J == "J")
+            {
+                Console.WriteLine(int.Parse(R) + int.Parse(L));
+            }
+            else if (R == "R")
+            {
+                Console.WriteLine(int.Parse(J) - int.Parse(L));
+            }
+            else
+            {
+                Console.WriteLine(int.Parse(J) - int.Parse(R));
+            }
+        }
+    }
+
+}
