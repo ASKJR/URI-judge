a88a7d23835be60b2ce07de01807504e18677ed8
Alberto Kato <albertokatojr@gmail.com>
2022-05-03 22:34:50 -0300

    [C#] 3307 - Decora o Rolê!

---
+++ C-Sharp/3307-Decora-o-Rolê!/3307-Decora-o-Rolê!.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Decora o Rolê!
+     *  beecrowd | 3307 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        double area, radius;
+        
+        for (int i = 0; i < N; i++)
+        {
+            area = double.Parse(Console.ReadLine());
+            radius = Math.Sqrt(area/12.56);
+            if (radius < 12)
+            {
+                Console.WriteLine(String.Format("vermelho = R$ {0:0.00}", area * 0.09));
+            } 
+            else if (radius >= 12 && radius <= 15)
+            {
+                Console.WriteLine(String.Format("azul = R$ {0:0.00}", area * 0.07));
+            }
+            else
+            {
+                Console.WriteLine(String.Format("amarelo = R$ {0:0.00}", area * 0.05));
+            }
+        }
+    }
+}
