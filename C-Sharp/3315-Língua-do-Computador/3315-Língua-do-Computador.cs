b594bf8506763db4da3901e622623cb8065273de
Alberto Kato <albertokatojr@gmail.com>
2022-05-03 23:03:14 -0300

    [C#] 3315 -  Língua do Computador

---
+++ C-Sharp/3315-Língua-do-Computador/3315-Língua-do-Computador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Língua do Computador
+     *  beecrowd | 3315 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        List<int> count = new List<int>();
+        int max;
+
+        for (int i = 0; i < 4; i++)
+        {
+            count.Add(Console.ReadLine().Split(' ').Select(n => int.Parse(n)).Sum());
+        }
+
+        max = count.Max();
+
+        Console.WriteLine($"{max} = {Convert.ToString(max,2)}");
+    }
+}
