2cb5db394eb7f6ff81f8569f56fc21b5c0ea96cc
Alberto Kato <albertokatojr@gmail.com>
2022-05-03 21:52:38 -0300

    [C#] 1367 - Ajude!

---
+++ C-Sharp/1367-Ajude!/1367-Ajude!.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+class URI
+{
+    /*  
+     *  Problem: Ajude!
+     *  beecrowd | 1367 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int N, correct, time;
+        string[] input;
+        List<string> correctLetter;
+        List<string> incorrectLetter;
+
+        while (true)
+        {
+            N = int.Parse(Console.ReadLine());
+            
+            if (N == 0) break;
+
+
+            correct = 0;
+            time = 0;
+            correctLetter = new List<string>();
+            incorrectLetter = new List<string>();
+
+            for (int i = 0; i < N; i++)
+            {
+                input = Console.ReadLine().Split(' ');
+
+                if (input[2] == "correct")
+                {
+                    if (!correctLetter.Contains(input[0]))
+                    {
+                        correctLetter.Add(input[0]);
+                    }
+                    correct++;
+                    time+= int.Parse(input[1]);
+                } 
+                else
+                {
+                    incorrectLetter.Add(input[0]);
+                }
+            }
+
+            foreach (string letter in incorrectLetter)
+            {
+                if(correctLetter.Contains(letter))
+                {
+                    time += 20;
+                }
+            }
+
+            if (correct == 0) time = 0;
+
+            Console.WriteLine($"{correct} {time}");
+        }
+        
+    }
+}
