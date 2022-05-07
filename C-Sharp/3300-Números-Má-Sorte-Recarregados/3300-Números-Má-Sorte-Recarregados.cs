852db364fe2c6ab955594dd2bd78afff466eda06
Alberto Kato <albertokatojr@gmail.com>
2022-05-07 17:26:40 -0300

    [C#] 3300 - Números Má Sorte Recarregados

---
+++ C-Sharp/3300-Números-Má-Sorte-Recarregados/3300-Números-Má-Sorte-Recarregados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class URI
+{
+    /*  
+     *  Problem: Números Má Sorte Recarregados
+     *  beecrowd | 3300 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string N = Console.ReadLine();
+        if (N.Contains("13"))
+        {
+            Console.WriteLine($"{N} es de Mala Suerte");
+        } 
+        else
+        {
+            Console.WriteLine($"{N} NO es de Mala Suerte");
+        }
+
+    }
+}
