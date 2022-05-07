98171dd50c82524e578cf2a0660fb7706552be92
Alberto Kato <albertokatojr@gmail.com>
2022-05-07 17:04:53 -0300

    [C#] 2456 - Cartas

---
+++ C-Sharp/2456-Cartas/2456-Cartas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Cartas
+     *  beecrowd | 2456 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split(' ').Select(n => int.Parse(n)).ToArray();
+        int[] ascending = input.OrderBy(l => l).ToArray();
+        int[] descending = input.OrderByDescending(l => l).ToArray();
+        
+
+        if (input.SequenceEqual(ascending))
+        {
+            Console.WriteLine("C");
+        } 
+        else if (input.SequenceEqual(descending))
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("N");
+        }
+
+    }
+}
