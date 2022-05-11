6776929d3c1b63fce5e95cd4e0aa0d13ce24d438
Alberto Kato <albertokatojr@gmail.com>
2022-05-10 22:08:32 -0300

    [C#] 2023 - A Última Criança Boa

---
+++ C-Sharp/2023-A-Última-Criança-Boa/2023-A-Última-Criança-Boa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: A Última Criança Boa
+     *  beecrowd | 2023 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        string line;
+        SortedDictionary<string,string> dic = new SortedDictionary<string,string>();
+        
+        while ((line = Console.ReadLine()) != null)
+        {
+            if (!dic.ContainsKey(line.ToLower()))
+            {
+                dic.Add(line.ToLower(), line);
+            }
+            
+        }
+
+        Console.WriteLine(dic.Last().Value);
+    }
+}
