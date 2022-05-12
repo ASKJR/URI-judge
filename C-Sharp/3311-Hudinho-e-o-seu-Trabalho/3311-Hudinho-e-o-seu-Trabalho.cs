534853fcce60dfe8adbf523efac47994bb4c52c0
Alberto Kato <albertokatojr@gmail.com>
2022-05-12 20:31:03 -0300

    [C#] 3311 - Hudinho e o seu Trabalho

---
+++ C-Sharp/3311-Hudinho-e-o-seu-Trabalho/3311-Hudinho-e-o-seu-Trabalho.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class URI
+{
+    /*  
+     *  Problem: Hudinho e o seu Trabalho
+     *  beecrowd | 3311 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string name, firstLetter;
+        SortedDictionary<string, List<string>> names = new SortedDictionary<string, List<string>>();
+
+        for (int i = 0; i < N; i++)
+        {
+            name = Console.ReadLine();
+            firstLetter = name.Substring(0, 1);
+            if (names.ContainsKey(firstLetter))
+            {
+                names[firstLetter].Add(name);
+            }
+            else
+            {
+                names.Add(firstLetter, new List<string>());
+                names[firstLetter].Add(name);
+            }
+        }
+        foreach (List<string> entries in names.Values)
+        {
+            foreach (string entry in entries)
+            {
+                Console.WriteLine(entry);
+            }
+        }
+    }
+}
