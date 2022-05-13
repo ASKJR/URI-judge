197c7122c5c5f3ffe7632871fe9762bf99566851
Alberto Kato <albertokatojr@gmail.com>
2022-05-12 22:56:04 -0300

    [C#] 3126 - Lista de Treinamento

---
+++ C-Sharp/3126-Lista-de-Treinamento/3126-Lista-de-Treinamento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class URI
+{
+    /*  
+     *  Problem: Lista de Treinamento
+     *  beecrowd | 3126 
+     *  Solution developed by: Alberto Kato
+     */
+
+    static void Main(string[] args)
+    {
+
+        string N = Console.ReadLine();
+        Console.WriteLine(Console.ReadLine().Split(' ').Select(n => int.Parse(n)).Sum()); 
+    }
+}
